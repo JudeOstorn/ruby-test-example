57bebcdb6209ec4f937beac8eac0942f53f8bab4
JudeOstorn <judeostorn66@gmail.com>
2017-10-04 12:11:52 +0500

    update random banner

--- random_banner.rb
+++ random_banner.rb
@@ -1,4 +1,20 @@
-# представил выполнение в rails...
-array.shuffle.each do |f|
-  p f
+
+def output_banner(array: [1,2,3,4,5], n: 5)
+  i = 0
+  while i < n
+    a = array.shuffle if i == 0
+    p a[i]
+    i+=1
+  end
+  return a
 end
+
+n = 30
+array = (1..n).to_a
+
+a = output_banner(array: array, n: n)
+p "первый прогон: #{a}"
+a = output_banner(array: array, n: n)
+p "второй прогон: #{a}"
+p "второй может начаться с того чем закончился предыдущий. шансы равны для всех баннеров"
+p "но в условии надо прогнать всего один раз =) "
