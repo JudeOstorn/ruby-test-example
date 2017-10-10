0d720cda6477eb5fc4ea017edce5472b3d130a7e
judeostorn <judeostorn66@gmail.com>
2017-10-10 12:23:40 +0500

    refactor 5 quest

--- iptocountry.rb
+++ iptocountry.rb
@@ -1,7 +1,7 @@
 require 'ipaddr'
 
 module IpToCountry
-  IPS_ARRAY = []
+  IPS_ARRAY = [].freeze
 
   def self.find_country(ip)
     ip = ip[3] + (ip[2] * 256) + (ip[1] * 256**2) + (ip[0] * 256**3)
@@ -13,30 +13,6 @@ module IpToCountry
     end
   end
 
-=begin
-  def self.search_in_file(ip, file_name)
-    File.open(file_name, 'r') do |infile|
-      while (line = infile.gets)
-        next if line[0] == '#'
-        ip_from = line.split(',')[0].slice(1..-2).to_i
-        ip_to = line.split(',')[1].slice(1..-2).to_i
-
-        if ip.between?(ip_from, ip_to)
-          country = line.split(',')[4].slice(1..-2)
-          break
-        end
-      end
-      "Country: #{country.nil? ? 'not found' : country}"
-    end
-  rescue => exception
-    puts "Error! #{exception.inspect}"
-  end
-
-  def self.const_show
-    p IPS_ARRAY[0..4]
-  end
-=end
-
   def self.read_from_file(file_name)
     ips_fields_array = []
     ips_fields = File.readlines(file_name)
@@ -44,17 +20,12 @@ module IpToCountry
       next if line[0] == '#'
       ip_from = line.split(',')[0].slice(1..-2).to_i
       ip_to = line.split(',')[1].slice(1..-2).to_i
-      country = line.split(',')[4].slice(1..-2)
+      country = line.split(',')[6].slice(1..-2)
       IPS_ARRAY << [ip_from, ip_to, country]
     end
-    return IPS_ARRAY
+    IPS_ARRAY
   end
 end
 
 ips_array = IpToCountry.read_from_file('IpToCountry.csv')
 IpToCountry.find_country(ARGV.first.split('.').map(&:to_i))
-
-#IpToCountry.const_show
-#p ips_array[0..5]
-
-# IpToCountry.find_country(ARGV.first.split('.').map(&:to_i))#, 'IpToCountry.csv')
