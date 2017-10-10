56f7fe8a5a3588d88c85ec70fef97cfd566ac523
judeostorn <judeostorn66@gmail.com>
2017-10-10 13:29:30 +0500

    refactor 5 quest with csv gem

--- iptocountry.rb
+++ iptocountry.rb
@@ -1,4 +1,5 @@
 require 'ipaddr'
+require 'csv'
 
 module IpToCountry
   IPS_ARRAY = []
@@ -14,17 +15,17 @@ module IpToCountry
 
   def self.read_from_file(file_name)
     ips_fields_array = []
-    ips_fields = File.readlines(file_name)
+    ips_fields = CSV.read(file_name, quote_char: "\x00", skip_lines: '\#')
     ips_fields.each do |line|
       next if line[0] == '#'
-      ip_from = line.split(',')[0].slice(1..-2).to_i
-      ip_to = line.split(',')[1].slice(1..-2).to_i
-      country = line.split(',')[6].slice(1..-2)
-      IPS_ARRAY << [ip_from, ip_to, country.chop]
+      ip_from = line[0].slice(1..-2).to_i
+      ip_to = line[1].slice(1..-2).to_i
+      country = line[6].slice(1..-2)
+      IPS_ARRAY << [ip_from, ip_to, country]
     end
     IPS_ARRAY.freeze
   end
 end
 
 ips_array = IpToCountry.read_from_file('IpToCountry.csv')
-IpToCountry.find_country(ip = IPAddr.new(ARGV.join(" ")).to_i())
+IpToCountry.find_country(ip = IPAddr.new(ARGV.join(' ')).to_i)
