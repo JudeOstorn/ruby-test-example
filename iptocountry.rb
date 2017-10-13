b84665a730651a0ecc99727926e15678869d2acb
judeostorn <judeostorn66@gmail.com>
2017-10-13 19:00:55 +0500

    refactor self

--- iptocountry.rb
+++ iptocountry.rb
@@ -12,23 +12,25 @@ class Entry
 end
 
 module IpToCountry
-  def self.read_from_file(file_name)
-    array = []
-    CSV.foreach(file_name, quote_char: "\x00", skip_lines: '\#') do |row|
-      range = (row[0].slice(1..-2).to_i)..(row[1].slice(1..-2).to_i)
-      country = row[6].slice(1..-2)
-      array << Entry.new(range, country)
+  class << self
+    def read_from_file(file_name)
+      array = []
+      CSV.foreach(file_name, quote_char: "\x00", skip_lines: '\#') do |row|
+        range = (row[0].slice(1..-2).to_i)..(row[1].slice(1..-2).to_i)
+        country = row[6].slice(1..-2)
+        array << Entry.new(range, country)
+      end
+      array
     end
-    array
-  end
 
-  def self.binary_search(ips: nil, ip: 16_909_060)
-    if ips.size == 1
-      ips.first
-    elsif ip < ips[ips.size / 2 - 1].range.last
-      binary_search(ips: ips[0..ips.size / 2 - 1], ip: ip)
-    elsif ip > ips[ips.size / 2].range.first
-      binary_search(ips: ips[ips.size / 2..ips.size], ip: ip)
+    def binary_search(ips: nil, ip: 16_909_060)
+      if ips.size == 1
+        ips.first
+      elsif ip < ips[ips.size / 2 - 1].range.last
+        binary_search(ips: ips[0..ips.size / 2 - 1], ip: ip)
+      elsif ip > ips[ips.size / 2].range.first
+        binary_search(ips: ips[ips.size / 2..ips.size], ip: ip)
+      end
     end
   end
 end
