8c2e80127b2c92f2bfb14050e48ca37af4900b36
judeostorn <judeostorn66@gmail.com>
2017-10-13 18:38:00 +0500

    refactor iptocountry.rb array->hash

--- iptocountry.rb
+++ iptocountry.rb
@@ -2,18 +2,17 @@ require 'ipaddr'
 require 'csv'
 
 module IpToCountry
-  IPS_HASH = {}
-
   def self.read_from_file(file_name)
+    hash = {}
     CSV.foreach(file_name, skip_lines: '\#') do |row|
       range = (row[0].to_i)..(row[1].to_i)
       country = row[6]
-      IPS_HASH.store(range, country)
+      hash.store(range, country)
     end
-    IPS_HASH.freeze
+    hash
   end
 
-  def self.binary_search(ips: IPS_HASH.keys[0..IPS_HASH.size - 1], ip: 16_909_060)
+  def self.binary_search(ips: nil, ip: 16_909_060)
     if ips.size == 1
       ips
     elsif ip < ips[ips.size / 2 - 1].last
@@ -24,12 +23,10 @@ module IpToCountry
   end
 end
 
-begin
-  IpToCountry.read_from_file('IpToCountry.csv')
+
+  hash = IpToCountry.read_from_file('IpToCountry.csv')
   ip = IPAddr.new(ARGV.join(' ')).to_i
   p "Мы ищем: #{ARGV.join(' ')} <=> #{ip}"
-  p result = IpToCountry.binary_search(ip: ip)
-  p "Искомый ip находится в #{IpToCountry::IPS_HASH[result[0]]}"
-rescue => exception
-  puts "Error! #{exception.inspect}"
-end
+  p result = IpToCountry.binary_search(ips: hash.keys[0..hash.size - 1], ip: ip)[0]
+  p result.class
+  p "Искомый ip находится в #{hash[result]} "
